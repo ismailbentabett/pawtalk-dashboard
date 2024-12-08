7de7f9bcd9d1562c73be4ad041aff9279e95d498
Ismail Bentabet <ismailbentabett@gmail.com>
2024-12-09 00:15:26 +0100

    add pet management constants; define pagination and initial filter settings for pet listings

---
+++ src/constants/petManagement.ts
@@ -0,0 +1,12 @@
+import { PetFilters } from "../types/pet";
+
+export const PETS_PER_PAGE = 10;
+
+export const INITIAL_FILTERS: PetFilters = {
+  species: [],
+  status: [],
+  ageRange: { min: 0, max: 100 },
+  matchRateThreshold: 0,
+  tags: [],
+};
+
